7a7640f4952e7f09c8ed9a581aa36a313b46ad0c
Dragos Sarpe <contact@usdgadget.com>
2018-12-05 11:08:32 +0200

    Added a Recipe Model and a List of Recipes

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,13 @@
+// We want to define how a recipe should look like in this class
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+}
