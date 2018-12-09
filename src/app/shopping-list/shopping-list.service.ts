4ca763291c4aa4f341ad5c070bffa9e803ff10e9
Dragos Sarpe <contact@usdgadget.com>
2018-12-09 11:05:34 +0200

    Added the Shopping List Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,18 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
-    
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ];
+
+    getIngredients() {
+        // We'll return the ingredients, but only a copy of it with slice method. So that I can't access
+        // the original array stored in the service.
+        return this.ingredients.slice();  
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+    }
 }
