c80f15cee9669110745468b621cc202d757505d0
Dragos Sarpe <contact@usdgadget.com>
2018-12-17 19:37:32 +0200

    Sending the token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,19 +4,26 @@ import { map } from 'rxjs/operators';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService) { }
+    constructor(private http: Http, 
+                private recipeService: RecipeService,
+                private authService: AuthService) { }
 
     storeRecipes() {
-        // We're using put method because we want to constantly override the old data on Firebase
-        return this.http.put('https://ng-recipe-book-3db1c.firebaseio.com/recipes.json', 
+        const token = this.authService.getToken();
+
+        // We're using PUT method because we want to constantly override the old data on Firebase
+        return this.http.put(`https://ng-recipe-book-3db1c.firebaseio.com/recipes.json?auth=${token}`, 
                       this.recipeService.getRecipes());   
     }
 
     getRecipes() {
-        this.http.get('https://ng-recipe-book-3db1c.firebaseio.com/recipes.json')
+        const token = this.authService.getToken();
+
+        this.http.get(`https://ng-recipe-book-3db1c.firebaseio.com/recipes.json?auth=${token}`)
             .pipe(map(
                 (response: Response) => {
                     const recipes: Recipe[] = response.json();
