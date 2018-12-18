226c24c37ed4df14e91b4109feab8e3190ebe638
Dragos Sarpe <contact@usdgadget.com>
2018-12-18 18:21:25 +0200

    Added a basic Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,30 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module';
-import { HomeComponent } from './home/home.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
     // DropdownDirective    // We delete DropdownDirective because we import the SharedModule
   ],
   imports: [
     BrowserModule,   // It contains all the features of the CommonModule and some additional features
     HttpModule,
     // RecipesModule,   // We delete it because we're using lazy loading
-    AppRoutingModule,
+    // AppRoutingModule,  // We delete it because we export it in CoreModule 
     SharedModule,
     ShoppingListModule,
-    AuthModule
+    AuthModule, 
+    CoreModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
