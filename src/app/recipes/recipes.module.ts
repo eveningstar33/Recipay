e55f2b4f348e0b8b32b98b0d186bc2e0a36fab56
Dragos Sarpe <contact@usdgadget.com>
2018-12-18 10:25:10 +0200

    Added a Recipe Feature Module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RecipesComponent } from './recipes.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+   declarations: [      // Here are the components
+       RecipesComponent,
+       RecipeStartComponent,
+       RecipeListComponent,
+       RecipeEditComponent,
+       RecipeDetailComponent, 
+       RecipeItemComponent
+   ],
+   imports: [    // Which other moddules do you want to use in this module?
+        CommonModule,   // It is used in every feature module, it gives acces to ngFor, ngIf
+        ReactiveFormsModule
+   ] 
+})
+export class RecipesModule {
+
+}
