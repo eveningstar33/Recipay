9e4a8ca4642ff71824bad81cdd594e014d8a12df
Dragos Sarpe <contact@usdgadget.com>
2018-12-18 13:40:47 +0200

    Added Auth Feature Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },   // Only redirect if the full path is empty
@@ -12,9 +10,7 @@ const appRoutes: Routes = [
   //   { path: ':id', component: RecipeDetailComponent },
   //   { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
   // ] },
-  { path: 'shopping-list', component: ShoppingListComponent },
-  { path: 'signup', component: SignupComponent },
-  { path: 'signin', component: SigninComponent }
+  { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
 @NgModule({
